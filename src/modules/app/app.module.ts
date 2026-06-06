f1f6ae6f96e2f4a3bad5d30443830548d3cd2cbf
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2026-06-06 16:41:48 +0330

    first commit

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+// import { AuthModule } from "../auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
+// import { TypeOrmDbConfig } from "src/config/typeorm.config";
+// import { UserModule } from "../user/user.module";
+import { HttpModule } from "@nestjs/axios";
+
+@Module({
+  imports: [
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    //   envFilePath: join(process.cwd(), ".env"),
+    // }),
+    // TypeOrmModule.forRootAsync({
+    //   // useClass: TypeOrmDbConfig,
+    // }),
+    // AuthModule,
+    // UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
